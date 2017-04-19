e08fe5902f7b6b877b50dc60eaddca298f86ff5f
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-18 17:59:38 -0700

    added buttons

--- client/components/Header.js
+++ client/components/Header.js
@@ -3,13 +3,29 @@ import { graphql } from 'react-apollo';
 import query from '../queries/CurrentUser';
 
 class Header extends Component {
-    render() {
-        return (
-            <div> 
-                Header 
-            </div>
-        );
-    }
+	renderButtons() {
+		const { loading, user } = this.props.data;
+
+		if (this.props.data.loading) { return <div />; }
+		
+		if (user) {
+			return	<div>Logout</div>;
+		} else {
+			return (
+				<div>You're not signed in.</div>
+			);
+		}
+	}
+
+	render() {
+		return (
+			<nav>
+				<div className="nav-wrapper"> 
+					{this.renderButtons()}
+				</div>
+			</nav>
+		);
+	}
 }
 
 export default graphql(query)(Header);
